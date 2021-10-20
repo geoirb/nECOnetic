27c862907feeb709cd6ef08ced31be4df2ebafd5
geoirb <morochev.g@gmail.com>
2021-10-20 11:36:56 +0000

    migrate eco data

--- data-service/tools/migrator/eco-data/main.go
+++ data-service/tools/migrator/eco-data/main.go
@@ -22,66 +22,66 @@ var sources []struct {
 	fileName    string
 	filePath    string
 }{
-	// {
-	// 	stationName: "Академика Анохина",
-	// 	fileName:    "Академика Анохина 2020.xlsx",
-	// 	filePath:    "../dataset/Академика Анохина 2020.xlsx",
-	// },
+	{
+		stationName: "Академика Анохина",
+		fileName:    "Академика Анохина 2020.xlsx",
+		filePath:    "../dataset/Академика Анохина 2020.xlsx",
+	},
 	{
 		stationName: "Бутлерова",
 		fileName:    "Бутлерова 2020.xlsx",
 		filePath:    "../dataset/Бутлерова 2020.xlsx",
 	},
-	// {
-	// 	stationName: "Глебовская",
-	// 	fileName:    "Глебовская 2020 год.xlsx",
-	// 	filePath:    "../dataset/Глебовская 2020 год.xlsx",
-	// },
-	// {
-	// 	stationName: "Коптевский",
-	// 	fileName:    "Коптевский бул. 2020 год.xlsx",
-	// 	filePath:    "../dataset/Коптевский бул. 2020 год.xlsx",
-	// },
-	// {
-	// 	stationName: "Марьино",
-	// 	fileName:    "Марьино 2020.xlsx",
-	// 	filePath:    "../dataset/Марьино 2020.xlsx",
-	// },
-	// {
-	// 	stationName: "Останкино",
-	// 	fileName:    "Останкино 0 2020 год.xlsx",
-	// 	filePath:    "../dataset/Останкино 0 2018 год.xlsx",
-	// },
-	// {
-	// 	stationName: "Останкино",
-	// 	fileName:    "Останкино 0 2020 год.xlsx",
-	// 	filePath:    "../dataset/Останкино 0 2019 год.xlsx",
-	// },
-	// {
-	// 	stationName: "Останкино",
-	// 	fileName:    "Останкино 0 2020 год.xlsx",
-	// 	filePath:    "../dataset/Останкино 0 2020 год.xlsx",
-	// },
-	// {
-	// 	stationName: "Пролетарский",
-	// 	fileName:    "Пролетарский проспект 2020.xlsx",
-	// 	filePath:    "../dataset/Пролетарский проспект 2020.xlsx",
-	// },
-	// {
-	// 	stationName: "Спиридоновка",
-	// 	fileName:    "Спиридоновка ул. 2020 год.xlsx",
-	// 	filePath:    "../dataset/Спиридоновка ул. 2020 год.xlsx",
-	// },
-	// {
-	// 	stationName: "Туристская",
-	// 	fileName:    "Туристская 2020 год.xlsx",
-	// 	filePath:    "../dataset/Туристская 2020 год.xlsx",
-	// },
-	// {
-	// 	stationName: "Шаболовка",
-	// 	fileName:    "Шаболовка 2020 год.xlsx",
-	// 	filePath:    "../dataset/Шаболовка 2020.xlsx",
-	// },
+	{
+		stationName: "Глебовская",
+		fileName:    "Глебовская 2020 год.xlsx",
+		filePath:    "../dataset/Глебовская 2020 год.xlsx",
+	},
+	{
+		stationName: "Коптевский",
+		fileName:    "Коптевский бул. 2020 год.xlsx",
+		filePath:    "../dataset/Коптевский бул. 2020 год.xlsx",
+	},
+	{
+		stationName: "Марьино",
+		fileName:    "Марьино 2020.xlsx",
+		filePath:    "../dataset/Марьино 2020.xlsx",
+	},
+	{
+		stationName: "Останкино",
+		fileName:    "Останкино 0 2020 год.xlsx",
+		filePath:    "../dataset/Останкино 0 2018 год.xlsx",
+	},
+	{
+		stationName: "Останкино",
+		fileName:    "Останкино 0 2020 год.xlsx",
+		filePath:    "../dataset/Останкино 0 2019 год.xlsx",
+	},
+	{
+		stationName: "Останкино",
+		fileName:    "Останкино 0 2020 год.xlsx",
+		filePath:    "../dataset/Останкино 0 2020 год.xlsx",
+	},
+	{
+		stationName: "Пролетарский",
+		fileName:    "Пролетарский проспект 2020.xlsx",
+		filePath:    "../dataset/Пролетарский проспект 2020.xlsx",
+	},
+	{
+		stationName: "Спиридоновка",
+		fileName:    "Спиридоновка ул. 2020 год.xlsx",
+		filePath:    "../dataset/Спиридоновка ул. 2020 год.xlsx",
+	},
+	{
+		stationName: "Туристская",
+		fileName:    "Туристская 2020 год.xlsx",
+		filePath:    "../dataset/Туристская 2020 год.xlsx",
+	},
+	{
+		stationName: "Шаболовка",
+		fileName:    "Шаболовка 2020 год.xlsx",
+		filePath:    "../dataset/Шаболовка 2020.xlsx",
+	},
 }
 
 func main() {
